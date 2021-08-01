59cdc4077d26f897a8677c1f5cbd66a466ba2791
Pulin Angurala <pulinangurala@Pulins-MBP.hub>
2021-08-01 23:28:05 +1200

    First commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
+import React, { Component } from 'react';
 
-const App = () => 
-    <div className="App">
-      <h1>Welcome to CISE - the home of learning and fun</h1>
-    </div>
+
+// const App = () => 
+//     <div className="App">
+//       <h1>Welcome to CISE - the home of learning and fun</h1>
+//     </div>
+
+// export default App;
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
+}
 
 export default App;
